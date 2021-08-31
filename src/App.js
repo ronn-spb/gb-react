31fcf31f64a3dacdf26c5bc6074ff6899be43fa7
ronn_spb <ronn_spb@mail.ru>
2021-08-31 12:23:26 +0300

    partal work HW3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
-
+import { makeStyles } from '@material-ui/core/styles';
 import MessageList from "./components/MessageList";
+import MessageInput from "./components/MessageInput";
+const useStyles = makeStyles(() => ({
+  appWraper:{
+    
 
+  },
+}));
 function App() {
-  
+  const [messagesArray, setMessagesArray] = useState([]);
+
+  const onSendMessage = (text) => {
+    setMessagesArray((prev) => [...prev, { text: text, author: "ddd", bot: false }]);
+  };
+  const onAddMessage = () => {
+    
+    if (messagesArray.length > 0 && !messagesArray[messagesArray.length - 1].bot) {
+      setTimeout(() => setMessagesArray((prev) => [...prev, { text: "Hi i'am Bot", author: "Bot", bot: true }]), 1000);
+    }
+
+  }
+
+  useEffect(() => {
+    onAddMessage();
+  }, [messagesArray]);
   return (
     <div className="App">
      
-      <MessageList text="test"/>
+      <MessageList
+      messagesArray={messagesArray}/>
      
-      
+      <MessageInput
+      funcOnSendMessage={onSendMessage}
+    />
     </div>
      
   );
--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,49 +1,20 @@
-import React, { useEffect, useState } from "react";
-import {uid} from 'react-uid';
-import Message  from '../components/Message';
+import { uid } from 'react-uid';
+import Message from '../components/Message';
 
-const MessageList = (props) =>{
-    const [messagesArray, setMessagesArray] = useState([]);
-    const [inputMessage, setInputMessage] = useState("");
-    const onSendMessage = () => {
-        setMessagesArray((prev) => [...prev, {text:inputMessage,author:"ddd",bot:false}]);
-        setInputMessage("");
-      };
-    const onAddMessage =()=>{
-      //if ()console.log(messagesArray[messagesArray.length-1].bot);
-       if (messagesArray.length>0 && !messagesArray[messagesArray.length-1].bot){ 
-        setTimeout(()=>setMessagesArray((prev) => [...prev, {text:"Hi i'am Bot",author:"Bot",bot:true}]),1000);}
-
-    }
-    
-      useEffect(()=>{
-      onAddMessage();
-    },[messagesArray])
-    return <div>  
-    <div class="messagewraper"> 
-        {messagesArray.map((message, i) => (
+const MessageList = (props) => {
+ 
+   return <div>
+    <div class="messagewraper">
+      {props.messagesArray.map((message, i) => (
         <div key={uid(i)}>
-            <Message
+          <Message
             text={message.text}
-            author={message.author}/>
+            author={message.author} />
         </div>
       ))}
-      </div>
-      <div>
-      <input
-          className="input"
-          value={inputMessage}
-          onChange={(e) => setInputMessage(e.target.value)}
-          onKeyDown={({ key }) => {
-            if (key === "Enter") {
-              
-              onSendMessage();
-            }
-          }}
-        />
-         <button onClick={onSendMessage}>Отправить</button>
-        </div>
-        </div>
+    </div>
+    
+  </div>
 }
 
 export default MessageList;
